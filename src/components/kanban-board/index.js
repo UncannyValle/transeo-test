fdfaf3d942da3a5d3909d04cbb42c6b25270067a
Julian Valle <julian.valle@enovational.com>
2022-04-12 12:56:08 -0600

    updated icons added disabled stages for first and last arrows

--- src/components/kanban-board/index.js
+++ src/components/kanban-board/index.js
@@ -4,14 +4,14 @@ import "./index.css";
 export default class KanbanBoard extends Component {
   constructor(props) {
     super(props);
-    // Each task is uniquely identified by its name. 
+    // Each task is uniquely identified by its name.
     // Therefore, when you perform any operation on tasks, make sure you pick tasks by names (primary key) instead of any kind of index or any other attribute.
     this.state = {
-        tasks: this.props.tasks
+      tasks: this.props.tasks,
     };
-    this.stagesNames = ['Backlog', 'To Do', 'Ongoing', 'Done'];
+    this.stagesNames = ["Backlog", "To Do", "Ongoing", "Done"];
   }
- 
+
   render() {
     const { tasks } = this.state;
 
@@ -24,35 +24,68 @@ export default class KanbanBoard extends Component {
       stagesTasks[stageId].push(task);
     }
 
+    const moveRight = () => {
+      console.log("right");
+    };
+    const moveLeft = () => {
+      console.log("left");
+    };
+
     return (
       <div className="mt-20 layout-column justify-content-center align-items-center">
         <div className="mt-50 layout-row">
-            {stagesTasks.map((tasks, i) => {
-                return (
-                    <div className="card outlined ml-20 mt-0" key={`${i}`}>
-                        <div className="card-text">
-                            <h4>{this.stagesNames[i]}</h4>
-                            <ul className="styled mt-50" data-testid={`stage-${i}`}>
-                                {tasks.map((task, index) => {
-                                    return <li className="slide-up-fade-in" key={`${i}${index}`}>
-                                      <div className="li-content layout-row justify-content-between align-items-center">
-                                        <span data-testid={`${task.name.split(' ').join('-')}-name`}>{task.name}</span>
-                                        <div className="icons">
-                                          <button className="icon-only x-small mx-2" data-testid={`${task.name.split(' ').join('-')}-back`}>
-                                            <i className="material-icons">arrow_back</i>
-                                          </button>
-                                          <button className="icon-only x-small mx-2" data-testid={`${task.name.split(' ').join('-')}-forward`}>
-                                            <i className="material-icons">arrow_forward</i>
-                                          </button>
-                                        </div>
-                                      </div>
-                                    </li>
-                                })}
-                            </ul>
-                        </div>
-                    </div>
-                )
-            })}
+          {stagesTasks.map((tasks, i) => {
+            return (
+              <div className="card outlined ml-20 mt-0" key={`${i}`}>
+                <div className="card-text">
+                  <h4>{this.stagesNames[i]}</h4>
+                  <ul className="styled mt-50" data-testid={`stage-${i}`}>
+                    {tasks.map((task, index) => {
+                      return (
+                        <li className="slide-up-fade-in" key={`${i}${index}`}>
+                          <div className="li-content layout-row justify-content-between align-items-center">
+                            <span
+                              data-testid={`${task.name
+                                .split(" ")
+                                .join("-")}-name`}
+                            >
+                              {task.name}
+                            </span>
+                            <div className="icons">
+                              <button
+                                className={`icon-only x-small mx-2 ${
+                                  i === 0 ? "disabled" : null
+                                }`}
+                                data-testid={`${task.name
+                                  .split(" ")
+                                  .join("-")}-back`}
+                                onClick={moveLeft}
+                              >
+                                <i className="material-icons">arrow_back</i>
+                              </button>
+                              <button
+                                className={`icon-only x-small mx-2 ${
+                                  i === stagesTasks.length - 1
+                                    ? "disabled"
+                                    : null
+                                }`}
+                                data-testid={`${task.name
+                                  .split(" ")
+                                  .join("-")}-forward`}
+                                onClick={moveRight}
+                              >
+                                <i className="material-icons">arrow_forward</i>
+                              </button>
+                            </div>
+                          </div>
+                        </li>
+                      );
+                    })}
+                  </ul>
+                </div>
+              </div>
+            );
+          })}
         </div>
       </div>
     );
